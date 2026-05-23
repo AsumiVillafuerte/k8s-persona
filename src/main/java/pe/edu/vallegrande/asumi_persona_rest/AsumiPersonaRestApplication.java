2ee09c9ca9e0d8569c49fd79cc987802611b1d59
AsumiVillafuerte <asumi.penafiel.villafuerte@vallegrande.edu.pe>
2026-05-22 22:08:21 -0500

    proyecto

---
+++ src/main/java/pe/edu/vallegrande/asumi_persona_rest/AsumiPersonaRestApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.asumi_persona_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsumiPersonaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsumiPersonaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/vallegrande/asumi_persona_rest/AsumiPersonaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.asumi_persona_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsumiPersonaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
